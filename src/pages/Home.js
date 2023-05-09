0bc99c5790d9f808c07c8e33da36ff01f9f3cefa
ocampojkq <ocampo.jessekit1@gmail.com>
2023-05-09 22:38:10 +0800

    adding qoutes messages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Home = () => {
+  const [quote, setQuote] = useState("");
+
+  useEffect(() => {
+    const fetchQuote = async () => {
+      const response = await fetch(
+        "https://api.quotable.io/random?tags=inspirational,famous-quotes"
+      );
+      const data = await response.json();
+      setQuote(data.content);
+    };
+    fetchQuote();
+  }, []);
+
   return (
-    <div class="conatiner text-center">
-      <h1 class="text-center pt-5 text-warning">
+    <div class="container text-center">
+      <h1 class="text-center pt-5 text-warning fw-bolder">
         Fortune Favours the Lucky Number
       </h1>
+      {quote && (
+        <div className="mt-3">
+          <blockquote className="blockquote">
+            <h4 className="mb-0 pt-5 fw-bold">{quote}</h4>
+          </blockquote>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/ThreeDigits.js
+++ src/pages/ThreeDigits.js
@@ -31,16 +31,56 @@ const ThreeDigits = () => {
     }
   };
 
+  const randomColor = () => {
+    const colors = ["red", "blue", "green", "orange", "purple", "teal"];
+    return colors[Math.floor(Math.random() * colors.length)];
+  };
+
+  const luckyQuotes = [
+    "Luck is what happens when preparation meets opportunity.",
+    "The only sure thing about luck is that it will change.",
+    "Shallow men believe in luck or in circumstance. Strong men believe in cause and effect.",
+    "I'm a greater believer in luck, and I find the harder I work the more I have of it.",
+    "Luck is not chance, it's toil. Fortune's expensive smile is earned.",
+    "I have found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often.",
+    "Luck is not something you can mention in the presence of self-made men.",
+    "The meeting of preparation with opportunity generates the offspring we call luck.",
+    "The only way to get lucky is to take risks.",
+    "Luck is a dividend of sweat. The more you sweat, the luckier you get.",
+  ];
+
+  const randomQuote =
+    luckyQuotes[Math.floor(Math.random() * luckyQuotes.length)];
+
+  const circleStyles = {
+    width: "160px",
+    height: "160px",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    color: "white",
+    backgroundColor: randomColor(),
+    margin: "auto",
+  };
+
   return (
-    <div class="conatiner text-center">
-      <h1 class="pt-5 text-danger circle-number">
-        {rolling ? "Rolling..." : num}
-      </h1>
-      <button className="btn btn-danger mt-3" onClick={handleClick}>
-        Roll the number
-      </button>
+    <div className="container text-center pt-5">
+      <div className="text-center">
+        <h1 className=" circle-number" style={circleStyles}>
+          {rolling ? "Rolling" : num}
+        </h1>
+        <button className="btn btn-danger mt-5" onClick={handleClick}>
+          Roll the number
+        </button>
+        <div className="mt-3 fw-bold pt-5">
+          <h4>{randomQuote}</h4>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default ThreeDigits;
+
+//  return Math.floor(Math.random() * (999 - 100 + 1) + 100);
