2b2b51def93681cce111d0d69937912b6364b3c9
ocampojkq <ocampo.jessekit1@gmail.com>
2023-05-11 01:38:56 +0800

    adding news api

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,12 @@ import Footer from "./Footer";
 
 const Home = () => {
   const [quote, setQuote] = useState("");
-  const [image, setImage] = useState("");
-  const accessKey = "bnatQQw7w1MOBbmFcmd9o4rX50BltsdgP3xf6xXXydo";
+  // const [image, setImage] = useState("");
+  const [news, setNews] = useState([]);
+
+  // const accessKey = "bnatQQw7w1MOBbmFcmd9o4rX50BltsdgP3xf6xXXydo";
+  const newsApiKey = "5e34a7f6ce7145c8b9c32fe50579ae60";
+  const newsApiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${newsApiKey}`;
 
   useEffect(() => {
     const fetchQuote = async () => {
@@ -15,36 +19,37 @@ const Home = () => {
       setQuote(data.content);
     };
 
-    const fetchImage = async () => {
-      const response = await fetch(
-        `https://api.unsplash.com/photos/random/?client_id=${accessKey}&orientation=landscape`
-      );
+    const fetchNews = async () => {
+      const response = await fetch(newsApiUrl);
       const data = await response.json();
-      if (data.urls && data.urls.regular) {
-        setImage(data.urls.regular);
-      } else {
-        setImage(
-          "https://via.placeholder.com/800x600?text=We%20couldn%27t%20find%20a%20photo"
-        );
-      }
+      setNews(data.articles);
     };
 
     fetchQuote();
-    fetchImage();
-  }, []);
+    fetchNews();
+  }, [newsApiUrl]);
 
   return (
     <div className="container text-center">
       <h1 className="text-center pt-3 text-warning fw-bolder">
         Fortune Favours the Lucky Number
       </h1>
-      <div className="mt-3 pt-3">
-        {image ? (
-          <img src={image} alt="Random" className="img-fluid rounded" />
-        ) : (
-          <p>We couldn't find a photo</p>
-        )}
-      </div>
+
+      {news.length > 0 ? (
+        <div className="mt-3 pt-5">
+          <h3 className="mb-5 fw-bolder">Latest News</h3>
+          {news.map((article) => (
+            <div key={article.title}>
+              <h5>{article.title}</h5>
+              <p>{article.description}</p>
+              <hr />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p>Loading news...</p>
+      )}
+
       {quote && (
         <div className="mt-3">
           <blockquote className="blockquote">
@@ -59,3 +64,27 @@ const Home = () => {
 };
 
 export default Home;
+
+// const fetchImage = async () => {
+//   const response = await fetch(
+//     `https://api.unsplash.com/photos/random/?client_id=${accessKey}&orientation=landscape`
+//   );
+//   const data = await response.json();
+//   if (data.urls && data.urls.regular) {
+//     setImage(data.urls.regular);
+//   } else {
+//     setImage(
+//       "https://via.placeholder.com/800x600?text=We%20couldn%27t%20find%20a%20photo"
+//     );
+//   }
+// };
+
+// fetchImage();
+
+/* <div className="mt-3 pt-3">
+{image ? (
+  <img src={image} alt="Random" className="img-fluid rounded" />
+) : (
+  <p>We couldn't find a photo</p>
+)}
+</div> */
